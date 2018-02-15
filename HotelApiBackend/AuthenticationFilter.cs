f5dae9465e9089782fe4c0eb0c3eb985c23f1e42
erik <eriksorensen88@gmail.com>
2018-02-15 16:49:47 +0100

    API key authentication filter for backend

---
+++ HotelApiBackend/AuthenticationFilter.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelApiBackend
+{
+    public class HeaderAuthenticationFilter : IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            try
+            {
+                if (!IsValid(context))
+                {
+                    context.Result = new UnauthorizedResult();
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                context.Result = new UnauthorizedResult();
+            }
+        }
+
+        private static bool IsValid(AuthorizationFilterContext context)
+        {
+            return GetApiKey(context) == "secret"; // TODO: load from configuration
+        }
+
+        private static string GetApiKey(AuthorizationFilterContext context)
+        {
+            var apiKey = context.HttpContext.Request.Headers["X-Api-Key"];
+            return apiKey.First();
+        }
+    }
+}
--- HotelApiBackend/Startup.cs
+++ HotelApiBackend/Startup.cs
@@ -28,7 +28,10 @@ namespace HotelApiBackend
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddMvc();
+            services.AddMvc(opts =>
+            {
+                opts.Filters.Add(typeof(HeaderAuthenticationFilter));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
