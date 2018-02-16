9a5d219173ab38f9644882165e6ece9374d3f89a
Kenneth Langedal <Kenneth.Langedal@hotmail.com>
2018-02-16 16:15:39 +0100

    .

--- HotelCore/ReseptionService.cs
+++ HotelCore/ReseptionService.cs
@@ -8,5 +8,23 @@ namespace HotelCore
 {
     class ReseptionService
     {
+        private dat154_18_1Entities dx;
+
+        public ReseptionService()
+        {
+            dx = new dat154_18_1Entities();
+        }
+
+        public List<Reservation> GetReservations(bool active)
+        {
+            return dx.Reservations.Where(res => !active || res.Room == null).ToList();
+        }
+
+        public List<Room> GetReadyRooms(String roomType)
+        {
+            return dx.Rooms.Where(r => r.RoomType.Name == roomType 
+            && r.Reservations.Where(res => res.End < DateTime.Now).Count() == 0 
+            && true).ToList(); //todo
+        }
     }
 }
