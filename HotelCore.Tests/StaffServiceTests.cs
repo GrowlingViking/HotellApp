e7ac31919f516f71ead88de19c98d72524b02f32
GrowlingViking <siggesg@hotmail.com>
2018-02-16 16:52:00 +0100

    Added staff tests

---
+++ HotelCore.Tests/StaffServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HotelCore.Tests
+{
+    [TestClass]
+    public class StaffServiceTests
+    {
+        private ServiceFactory factory;
+
+        [TestInitialize]
+        public void Init()
+        {
+            factory = new ServiceFactory();
+        }
+
+        [TestMethod]
+        public void NothingCrashes()
+        {
+            var repo = factory.GetStaffService();
+            var tasks = repo.GetTasks("Cleaning");
+            var notes = repo.GetNotes(2);
+        }
+    }
+}
