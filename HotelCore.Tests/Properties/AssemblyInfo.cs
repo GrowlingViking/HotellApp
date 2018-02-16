810d7a46d7365801a8bfd5a570b0b2676e2e7c3d
Erik Sørensen <eriksorensen88@gmail.com>
2018-02-16 15:01:52 +0100

    Add some integration tesets

---
+++ HotelCore.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HotelCore.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HotelCore.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7e2671d2-39b1-43e9-a0f2-755449a1d426")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HotelCore.Tests/SmokeTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HotelCore.Tests
+{
+    [TestClass]
+    public class SmokeTests
+    {
+        [TestMethod]
+        public void NothingCrashes()
+        {
+            var repo = new CustomerService();
+            var reservations = repo.GetReservations("Erik");
+        }
+    }
+}
