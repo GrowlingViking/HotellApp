2b0135842d067c55ae8ef0a44dd9629caeea38d4
Kenneth Langedal <Kenneth.Langedal@hotmail.com>
2018-02-16 14:41:33 +0100

    customerService test implement

--- HotelCore/CustomerService.cs
+++ HotelCore/CustomerService.cs
@@ -8,5 +8,16 @@ namespace HotelCore
 {
     public class CustomerService
     {
+        private dat154_18_1Entities dx;
+
+        public CustomerService()
+        {
+            dx = new dat154_18_1Entities();
+        }
+
+        public List<Reservation> getReseptions(String User)
+        {
+            return dx.Reservations.Where(x => x.User.UserName == User).ToList();
+        }
     }
 }
