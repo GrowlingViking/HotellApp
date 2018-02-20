97f1068924ceaaada6066f9c598c9cbd3a257454
Erik Sørensen <eriksorensen88@gmail.com>
2018-02-20 13:43:02 +0100

    Base web app with connection to database

---
+++ HotelWeb/Controllers/MVC/IndexController.cs
@@ -0,0 +1,21 @@
+﻿using HotelCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelWeb.Controllers.MVC
+{
+    public class IndexController : Controller
+    {
+        private ServiceFactory sf = new ServiceFactory();
+
+        [Route("")]
+        public ActionResult Index()
+        {
+            var roomCount = sf.GetReceptionService().GetRooms().Count();
+            return View(roomCount);
+        }
+    }
+}
---
+++ HotelWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HotelWeb.Global" Language="C#" %>
