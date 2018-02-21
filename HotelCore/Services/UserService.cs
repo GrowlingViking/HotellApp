d1daabda75520c4e47fef51309ee8dc27ee066be
Erik Sørensen <eriksorensen88@gmail.com>
2018-02-21 21:46:16 +0100

    Starting hotel web auth implementation

---
+++ HotelCore/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using HotelCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelCore.Services
+{
+    public class UserService
+    {
+        private HotelModelCf ctx;
+
+        public UserService(HotelModelCf ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public void CreateNewUser(string username, string password)
+        {
+            User user = new User
+            {
+                UserName = username,
+                Password = password
+            };
+            ctx.Users.Add(user);
+            ctx.SaveChanges();
+        }
+
+        public User FindUserByName(string username)
+        {
+            return ctx.Users.Where(u => u.UserName == username).First();
+        }
+
+        public bool UserExists(string username)
+        {
+            return ctx.Users.Where(u => u.UserName == username).Count() > 0;
+        }
+    }
+}
---
+++ HotelWeb/HotelAuthFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelWeb
+{
+    public class HotelAuthFilter : FilterAttribute, IAuthorizationFilter
+    {
+        private SecurityManager securityManager;
+
+        public HotelAuthFilter()
+        {
+            securityManager = new SecurityManager(new HotelCore.ServiceFactory());
+        }
+
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
